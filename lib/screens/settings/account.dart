9d71f58644f26c06f4b6e0f098a5d0946ee95f1d
Tolkynbayeva <workfortime37@gmail.com>
2025-03-20 05:17:21 +0500

    feat: user profile

--- lib/screens/settings/account.dart
+++ lib/screens/settings/account.dart
@@ -1,21 +1,33 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:personnel_management_flutter/models/user_profile/user_profile.dart';
+import 'package:personnel_management_flutter/widgets/button_save.dart';
+import 'package:personnel_management_flutter/widgets/text_field.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({super.key});
-
   @override
   State<AccountScreen> createState() => _AccountScreenState();
 }
 
 class _AccountScreenState extends State<AccountScreen> {
+  final _formKey = GlobalKey<FormState>();
   final _nameController = TextEditingController();
   final _surnameController = TextEditingController();
   final _emailController = TextEditingController();
   final _phoneController = TextEditingController();
   String _avatarPath = '';
+  bool _profileExists = false;
+
+  final phoneMask = MaskTextInputFormatter(
+    mask: '+# (###) ###-##-##',
+    filter: {"#": RegExp(r'\d')},
+  );
 
   @override
   void initState() {
@@ -25,10 +37,11 @@ class _AccountScreenState extends State<AccountScreen> {
 
   Future<void> _loadUserProfile() async {
     final box = Hive.box<UserProfile>('user_profile');
-    if (box.isNotEmpty) {
-      final user = box.getAt(0);
+    if (box.containsKey('user_profile')) {
+      final user = box.get('user_profile');
       if (user != null) {
         setState(() {
+          _profileExists = true;
           _avatarPath = user.avatarPath;
           _nameController.text = user.name;
           _surnameController.text = user.surname;
@@ -39,24 +52,66 @@ class _AccountScreenState extends State<AccountScreen> {
     }
   }
 
-  Future<void> _saveUserProfile() async {
-    final box = Hive.box<UserProfile>('user_profile');
-    if (box.isNotEmpty) {
-      final user = UserProfile(
-        avatarPath: _avatarPath,
-        name: _nameController.text.trim(),
-        surname: _surnameController.text.trim(),
-        email: _emailController.text.trim(),
-        phone: _phoneController.text.trim(),
-      );
-      await box.add(user);
+  Future<void> _pickAvatar() async {
+    final picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _avatarPath = image.path;
+      });
     }
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text('Профиль сохранён')));
   }
 
-  void _pickAvatar() async {
-    setState(() {});
+  Future<void> _saveUserProfile() async {
+    final box = Hive.box<UserProfile>('user_profile');
+    final user = UserProfile(
+      avatarPath: _avatarPath,
+      name: _nameController.text.trim(),
+      surname: _surnameController.text.trim(),
+      email: _emailController.text.trim(),
+      phone: _phoneController.text.trim(),
+    );
+    await box.put('user_profile', user);
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('Профиль сохранён')));
+    Navigator.pop(context);
+  }
+
+  void _confirmDeleteUserProfile(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: const Color(0xFFF2F5F7),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+        title: const Text('Удалить профиль пользователя?'),
+        content: const Text('Вы уверены, что хотите удалить этот профиль?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text(
+              'Отмена',
+              style: TextStyle(color: Color(0xFF252525)),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              _deleteUserProfile(context);
+            },
+            child: const Text(
+              'Удалить',
+              style: TextStyle(color: Color(0xFFFF3B30)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _deleteUserProfile(BuildContext context) async {
+    final box = Hive.box<UserProfile>('user_profile');
+    await box.delete('user_profile');
+    Navigator.pop(context);
+    Navigator.pop(context, true);
   }
 
   @override
@@ -73,96 +128,136 @@ class _AccountScreenState extends State<AccountScreen> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
+      body: Padding(
         padding: EdgeInsets.symmetric(vertical: 40, horizontal: 16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              height: 110,
-              width: 110,
-              child: Stack(
-                children: [
-                  CircleAvatar(
-                    radius: 65,
-                    // backgroundColor: Color(0xFFF2F5F7),
-                    backgroundColor: Colors.red,
-                    backgroundImage: _avatarPath.isNotEmpty
-                        ? AssetImage(_avatarPath)
-                        : AssetImage(
-                            'assets/images/default_avatar.png',
-                          ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      _buildAvatar(),
+                      SizedBox(height: 60),
+                      ErrorTextFormField(
+                        controller: _nameController,
+                        hintText: 'Имя',
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ошибка';
+                          }
+                          return null;
+                        },
+                      ),
+                      ErrorTextFormField(
+                        controller: _surnameController,
+                        hintText: 'Фамилия',
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ошибка';
+                          }
+                          return null;
+                        },
+                      ),
+                      ErrorTextFormField(
+                        controller: _emailController,
+                        hintText: 'E-mail',
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ошибка';
+                          }
+                          return null;
+                        },
+                      ),
+                      ErrorTextFormField(
+                        controller: _phoneController,
+                        hintText: 'Номер телефона',
+                        keyboardType: TextInputType.phone,
+                        inputFormatters: [phoneMask],
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ошибка';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
                   ),
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: InkWell(
-                      onTap: _pickAvatar,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color(0xFFF2F5F7),
-                          border: Border.all(color: Colors.grey.shade300),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: SvgPicture.asset(
-                          'assets/svg/edit-icon.svg',
-                          width: 13,
-                          height: 13,
-                        ),
+                ),
+              ),
+            ),
+            _profileExists
+                ? TextButton(
+                    onPressed: () {
+                      _confirmDeleteUserProfile(context);
+                    },
+                    child: Text(
+                      'Удалить аккаунт',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color(0xFFFF3B30),
                       ),
                     ),
                   )
-                ],
-              ),
-            ),
-            SizedBox(height: 60),
-            _buildTextField(
-              controller: _nameController,
-              label: 'Имя',
-            ),
-            _buildTextField(
-              controller: _surnameController,
-              label: 'Фамилия',
-            ),
-            _buildTextField(
-              controller: _emailController,
-              label: 'E-mail',
-            ),
-            _buildTextField(
-              controller: _phoneController,
-              label: 'Номер телефона',
-            ),
+                : ButtonSave(onPressed: _saveUserProfile)
           ],
         ),
       ),
     );
   }
 
-  Widget _buildTextField({
-    required TextEditingController controller,
-    required String label,
-  }) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 16),
-      decoration: BoxDecoration(
-        color: Colors.amber,
-        //color: Color(0xFFF2F5F7),
-        borderRadius: BorderRadius.circular(13),
-      ),
-      child: TextField(
-        controller: controller,
-        decoration: InputDecoration(
-          labelText: label,
-          floatingLabelBehavior: FloatingLabelBehavior.never,
-          labelStyle:
-              TextStyle(fontSize: 16, height: 1.4, color: Color(0xFF818181)),
-          contentPadding: EdgeInsets.all(16),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(13),
-            borderSide: BorderSide.none,
+  Widget _buildAvatar() {
+    final imageProvider = _avatarPath.isNotEmpty
+        ? FileImage(File(_avatarPath))
+        : const AssetImage('assets/images/default_avatar.png') as ImageProvider;
+
+    return Stack(
+      alignment: Alignment.bottomRight,
+      children: [
+        Container(
+          width: 110,
+          height: 110,
+          decoration: BoxDecoration(
+            color: Color(0xFFF2F5F7),
+            boxShadow: const [
+              BoxShadow(
+                color: Color(0x0D000000),
+                offset: Offset(0, 2),
+                blurRadius: 6,
+              ),
+            ],
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-      ),
+        InkWell(
+          onTap: _pickAvatar,
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color(0xFFF2F5F7),
+              boxShadow: const [
+                BoxShadow(
+                  color: Color(0x0D000000),
+                  offset: Offset(0, 2),
+                  blurRadius: 6,
+                ),
+              ],
+            ),
+            padding: const EdgeInsets.all(10),
+            child: SvgPicture.asset(
+              'assets/svg/edit-icon.svg',
+              width: 13,
+              height: 13,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,98 +1,168 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:personnel_management_flutter/models/user_profile/user_profile.dart';
 import 'package:personnel_management_flutter/screens/settings/account.dart';
 import 'package:personnel_management_flutter/screens/settings/feedback.dart';
 import 'package:personnel_management_flutter/screens/settings/information.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  late Box<UserProfile> _userProfileBox;
+
+  @override
+  void initState() {
+    super.initState();
+    _userProfileBox = Hive.box<UserProfile>('user_profile');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.symmetric(vertical: 40, horizontal: 16),
-      children: [
-        ListTile(
-          contentPadding: EdgeInsets.zero,
-          title: Text(
-            'Настройка профиля',
-            style: TextStyle(
-              fontSize: 16,
-              height: 1.4,
-              color: Color(0xFF252525),
-            ),
-          ),
-          trailing: Icon(
-            Icons.chevron_right,
-            size: 34,
-            color: Color(0xFFDEDEDF),
-          ),
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (ctx) => AccountScreen(),
+    return ValueListenableBuilder(
+      valueListenable: _userProfileBox.listenable(keys: ['user_profile']),
+      builder: (context, Box<UserProfile> box, _) {
+        final UserProfile? userProfile = _userProfileBox.get('user_profile');
+
+        return ListView(
+          padding: EdgeInsets.symmetric(vertical: 40, horizontal: 16),
+          children: [
+            if (userProfile != null) ...[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 110,
+                    height: 110,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF2F5F7),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0x0D000000),
+                          offset: Offset(0, 2),
+                          blurRadius: 6,
+                        ),
+                      ],
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: userProfile.avatarPath.isNotEmpty
+                            ? FileImage(File(userProfile.avatarPath))
+                            : AssetImage('assets/images/default_avatar.png')
+                                as ImageProvider,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    '${userProfile.name} ${userProfile.surname}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      color: Color(0xFF252525),
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                ],
+              )
+            ],
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text(
+                'Настройка профиля',
+                style: TextStyle(
+                  fontSize: 16,
+                  height: 1.4,
+                  color: Color(0xFF252525),
+                ),
               ),
-            );
-          },
-        ),
-        Divider(
-          thickness: 0.5,
-          color: Color(0x40ADADAF),
-        ),
-        ListTile(
-          contentPadding: EdgeInsets.zero,
-          title: Text(
-            'Обратная связь',
-            style: TextStyle(
-              fontSize: 16,
-              height: 1.4,
-              color: Color(0xFF252525),
-            ),
-          ),
-          trailing: Icon(
-            Icons.chevron_right,
-            size: 34,
-            color: Color(0xFFDEDEDF),
-          ),
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (ctx) => FeedbackScreen(),
+              trailing: Icon(
+                Icons.chevron_right,
+                size: 34,
+                color: Color(0xFFDEDEDF),
               ),
-            );
-          },
-        ),
-        Divider(
-          thickness: 0.5,
-          color: Color(0x40ADADAF),
-        ),
-        ListTile(
-          contentPadding: EdgeInsets.zero,
-          title: Text(
-            'Информация',
-            style: TextStyle(
-              fontSize: 16,
-              height: 1.4,
-              color: Color(0xFF252525),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) => AccountScreen(),
+                  ),
+                );
+              },
             ),
-          ),
-          trailing: Icon(
-            Icons.chevron_right,
-            size: 34,
-            color: Color(0xFFDEDEDF),
-          ),
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (ctx) => InformationScreen(),
+            Divider(
+              thickness: 0.5,
+              color: Color(0x40ADADAF),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text(
+                'Обратная связь',
+                style: TextStyle(
+                  fontSize: 16,
+                  height: 1.4,
+                  color: Color(0xFF252525),
+                ),
               ),
-            );
-          },
-        ),
-        Divider(
-          thickness: 0.5,
-          color: Color(0x40ADADAF),
-        ),
-      ],
+              trailing: Icon(
+                Icons.chevron_right,
+                size: 34,
+                color: Color(0xFFDEDEDF),
+              ),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) => FeedbackScreen(),
+                  ),
+                );
+              },
+            ),
+            Divider(
+              thickness: 0.5,
+              color: Color(0x40ADADAF),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text(
+                'Информация',
+                style: TextStyle(
+                  fontSize: 16,
+                  height: 1.4,
+                  color: Color(0xFF252525),
+                ),
+              ),
+              trailing: Icon(
+                Icons.chevron_right,
+                size: 34,
+                color: Color(0xFFDEDEDF),
+              ),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) => InformationScreen(),
+                  ),
+                );
+              },
+            ),
+            Divider(
+              thickness: 0.5,
+              color: Color(0x40ADADAF),
+            ),
+          ],
+        );
+      },
     );
   }
 }
